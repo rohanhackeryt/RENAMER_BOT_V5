e153ccc76d4175581f071e88d3a47d1ecec6a821
GreyMatter's <greymattersyt@gmail.com>
2023-01-10 20:34:13 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
-ADMIN = int(os.environ.get("ADMIN", 923943045))
+ADMIN = int(os.environ.get("ADMIN", 5151412494))
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 
 
@@ -21,28 +21,16 @@ async def warn(c, m):
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.command(["addpremium"]))
 async def buypremium(bot, message):
 	await message.reply_text("Select Plan.........",quote=True,reply_markup=InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("VIP 1",callback_data = "vip1"), 
-        			InlineKeyboardButton("VIP 2",callback_data = "vip2") ]]))
+        			InlineKeyboardButton("VIP",callback_data = "vip") ]]))
         			
 
-@Client.on_callback_query(filters.regex('vip1'))
-async def vip1(bot,update):
+@Client.on_callback_query(filters.regex('vip'))
+async def vip(bot,update):
 	id = update.message.reply_to_message.text.split("/addpremium")
 	user_id = id[1].replace(" ", "")
-	inlimit  = 10737418240
-	uploadlimit(int(user_id),10737418240)
-	usertype(int(user_id),"VIP1")
+	inlimit  = 10737418240000
+	uploadlimit(int(user_id),10737418240000)
+	usertype(int(user_id),"VIP")
 	addpre(int(user_id))
-	await update.message.edit("Added successfully To Premium Upload limit 10 GB")
-	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 1 check your plan here /myplan")
-
-@Client.on_callback_query(filters.regex('vip2'))
-async def vip2(bot,update):
-	id = update.message.reply_to_message.text.split("/addpremium")
-	user_id = id[1].replace(" ", "")
-	inlimit  = 53687091200
-	uploadlimit(int(user_id),53687091200)
-	usertype(int(user_id),"VIP2")
-	addpre(int(user_id))
-	await update.message.edit("Added successfully To Premium Upload limit 50 GB")
-	await bot.send_message(user_id,"Hey Ur Upgraded To VIP 2 check your plan here /myplan")
+	await update.message.edit("Added successfully To Premium Users")
+	await bot.send_message(user_id,"Hey Ur Upgraded To VIP check your plan here /myplan")
